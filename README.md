86ad749b62fefb303b5ac1d8fcf25bb8b6825870
PARI <parimayamax@gmail.com>
2018-12-21 11:04:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ui testing with Puppeteer
+
+## Introduction
+
+> Code samples of my talk about Puppeteer. Slides in https://docs.google.com/presentation/d/1EaLmrgG9wIRg8A7r1oCU_UMztOoMXcN7KlR3Cg6VKDs/edit?usp=sharing (pt-br).
+
+## Up and running
+
+> `npm install` to install all dependencies. `npm test` will run all the tests.
---
+++ utils.js
@@ -0,0 +1,47 @@
+// module.exports.login_page = 'https://10.115.32.232';
+// module.exports.home_page = 'https://10.115.32.232/app';
+// module.exports.login_page = 'https://localhost:3030';
+// module.exports.home_page = 'https://localhost:3030/app';
+
+// module.exports.valid_email = 'admin';
+// module.exports.valid_password = 'FMSystest123!';
+
+module.exports.login_page = 'https://192.203.0.2';
+module.exports.home_page = 'https://192.203.0.2/app';
+module.exports.valid_email = 'admin';
+module.exports.valid_password = 'admin123A!!';
+
+
+
+// module.exports.login_page = 'https://10.115.32.196';
+// module.exports.home_page = 'https://10.115.32.196/app';
+// module.exports.valid_email = 'admin';
+// module.exports.valid_password = 'P0peye123!';
+
+// module.exports.login_page = 'https://10.115.40.100';
+// module.exports.home_page = 'https://10.115.40.100/app';
+// module.exports.valid_email = 'admin';
+// module.exports.valid_password = 'admin123A!';
+
+
+// module.exports.login_page = 'https://10.115.46.197';
+// module.exports.home_page = 'https://10.115.46.197/app';
+
+// module.exports.valid_email = 'admin';
+// module.exports.valid_password = 'ganesh123A!!';
+
+// module.exports.valid_email = 'admin';
+// module.exports.valid_password = 'FMSystest123!';
+//module.exports.valid_password = 'P0peye123!';
+
+
+
+// module.exports.login_page = 'https://10.115.40.100';
+// module.exports.home_page = 'https://10.115.40.100/app';
+// module.exports.valid_email = 'admin';
+// module.exports.valid_password = 'admin123A!';
+
+// module.exports.login_page = 'https://10.115.46.15';
+// module.exports.home_page = 'https://10.115.46.15/app';
+// module.exports.valid_email = 'admin';
+// module.exports.valid_password = 'admin123A!!';
